3aec2a45fe0913ba700e59df377ee551ff9e4472
Miguelantonio Guerra <mguerra502@gmail.com>
2019-05-31 01:14:09 +0100

    Server App

---
+++ server.py
@@ -0,0 +1,66 @@
+# Websocket server example that synchronizes state acress clients
+
+import asyncio
+import json
+import logging
+import websockets
+
+logging.basicConfig()
+
+state = {'value': 0}
+
+users = set()
+
+def state_event():
+	return json.dumps({'type': 'state', **state})
+
+
+def users_event():
+	return json.dumps({'type': 'users', 'count': len(users)})
+
+
+async def notify_state():
+	# Do not accept an empty list
+	if users:
+		message = state_event()
+		await asyncio.wait([user.send(message) for user in users])
+
+
+async def notify_users():
+	# Do not accept an empty list
+	if users:
+		message = users_event()
+		await asyncio.wait([user.send(message) for user in users])
+
+
+async def register(websocket):
+	users.add(websocket)
+	await notify_users()
+
+
+async def unregister(websocket):
+	users.remove(websocket)
+	await notify_users()
+
+
+async def counter(websocket, path):
+	# register(websocket) sends user_event() to websocket
+	await register(websocket)
+	try:
+		await websocket.send(state_event())
+		async for message in websocket:
+			data =  json.loads(message)
+			if data['action'] == 'minus':
+				state['value'] -= 1
+				await notify_state()
+			elif data['action'] == 'plus':
+				state['value'] += 1
+				await notify_state()
+			else:
+				logging.error(f"Unsupported event: {data}")
+	finally:
+		await unregister(websocket)
+
+
+asyncio.get_event_loop().run_until_complete(websockets.serve(counter, 'localhost', 6789))
+asyncio.get_event_loop().run_forever()
