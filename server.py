a355ad9ded0feda3be8aa0cc68bc7c705e585f00
Miguelantonio Guerra <mguerra502@gmail.com>
2019-05-31 01:41:35 +0100

    Test

--- server.py
+++ server.py
@@ -62,5 +62,5 @@ async def counter(websocket, path):
 		await unregister(websocket)
 
 
-asyncio.get_event_loop().run_until_complete(websockets.serve(counter, 'localhost', 6789))
+asyncio.get_event_loop().run_until_complete(websockets.serve(counter, 'https://ecoextpiapp.herokuapp.com/', 6789))
 asyncio.get_event_loop().run_forever()
