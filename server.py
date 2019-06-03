7c976bb0cb219a2e4995a69be2b06d6b8dd69aca
Miguelantonio Guerra <mguerra502@gmail.com>
2019-06-03 01:52:21 +0100

    Finding info

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ async def notify_users():
 async def register(websocket):
 	# print("#################################################")
 	print(sha512(str(websocket.__dict__).encode('utf-8')).hexdigest(), "\n")
-	pprint(str(websocket["Headers"]))
+	pprint(str(websocket["request_headers"]))
 	await notify_users()
 
 
